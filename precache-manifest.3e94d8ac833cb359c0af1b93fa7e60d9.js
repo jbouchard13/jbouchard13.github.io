8690bdccccc46eb3d7299ecb208cb11fe034e294
John Bouchard <jbouchard13@gmail.com>
2020-08-17 01:53:30 -0400

    Updates

---
+++ precache-manifest.3e94d8ac833cb359c0af1b93fa7e60d9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "607fcaa39d5fda48a731347759b5c896",
+    "url": "/index.html"
+  },
+  {
+    "revision": "395ea47d6613909dec32",
+    "url": "/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "cb04cdd06864cfa4f986",
+    "url": "/static/css/main.85173249.chunk.css"
+  },
+  {
+    "revision": "395ea47d6613909dec32",
+    "url": "/static/js/2.6707764e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6707764e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb04cdd06864cfa4f986",
+    "url": "/static/js/main.89f57f3a.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "bb0b79bc68dc4cd54501a4f35132f6b0",
+    "url": "/static/media/background.bb0b79bc.jpg"
+  }
+]);
