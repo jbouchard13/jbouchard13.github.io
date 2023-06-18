11aa44a16b1e6bdaf4c81eca0822441b9d017a6b
John Bouchard <jbouchard13@gmail.com>
2023-06-18 18:52:50 -0400

    updated links

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import links from '../../data/links.json';
-import NavLi from '../NavLi';
-import './style.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import links from "../../data/links.json";
+import NavLi from "../NavLi";
+import "./style.css";
 
 export default function Nav() {
   return (
-    <nav className='nav-bar'>
-      <ul className='nav-bar-nav'>
-        <li className='nav-logo'>
-          <Link to='/' className='nav-bar-link'>
-            <span className='link-text logo-text'>Links</span>
-            <i className='fas fa-angle-double-right logo-icon'></i>
+    <nav className="nav-bar">
+      <ul className="nav-bar-nav">
+        <li className="nav-logo">
+          <Link to="/" className="nav-bar-link">
+            <span className="link-text logo-text">Links</span>
+            <i className="fas fa-angle-double-right logo-icon"></i>
           </Link>
         </li>
         {links.map((link) => (
@@ -21,10 +21,10 @@ export default function Nav() {
             linkName={link.linkName}
           />
         ))}
-        <li className='nav-bar-item'>
-          <Link className='nav-bar-link' to='/contacts'>
-            <i className='far fa-envelope-open icon'></i>
-            <span className='link-text'>Contacts</span>
+        <li className="nav-bar-item">
+          <Link className="nav-bar-link" to="/contacts">
+            <i className="far fa-envelope-open icon"></i>
+            <span className="link-text">Contacts</span>
           </Link>
         </li>
       </ul>
--- src/components/NavLi/index.js
+++ src/components/NavLi/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 
 export default function NavLi(props) {
   return (
-    <li className='nav-bar-item'>
-      <a className='nav-bar-link' href={props.linkRoute} target=''>
+    <li className="nav-bar-item">
+      <a className="nav-bar-link" href={props.linkRoute} target="_blank">
         <i className={props.icon}></i>
-        <span className='link-text'>{props.linkName}</span>
+        <span className="link-text">{props.linkName}</span>
       </a>
     </li>
   );
